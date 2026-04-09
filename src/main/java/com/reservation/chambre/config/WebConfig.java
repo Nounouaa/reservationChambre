e81c8219a086dab9c63107aae09bb7781a8017d8
Nounouaa <lovamamy61@gmail.com>
2026-04-10 02:17:55 +0300

    ajout

--- src/main/java/com/reservation/chambre/config/WebConfig.java
+++ src/main/java/com/reservation/chambre/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
@@ -14,7 +13,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://reservationchambre.netlify.app") // ton Angular sur Netlify
+                        .allowedOrigins(
+                            "https://projetstages.netlify.app",
+                            "https://reservationchambre.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
